9edda471af99a3ab8c5f41765175285723658e0b
Moonmetro <64722102+Moonmetro@users.noreply.github.com>
2023-04-07 14:13:51 +0800

    Update animation-change.js

--- animation-change.js
+++ animation-change.js
@@ -1,9 +1,12 @@
-var CharacterArrray = ["A","B","C","D","E","F","G","H","I","J","K",
-"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-"a","b","c","d","e","f","g","h","i","j","k",
-"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-"0","1","2","3","4","5","6","7","8","9",
-"?",",","@","!","#","%","*",";","&"
+var CharacterArrray = [
+    
+    "A","B","C","D","E","F","G","H","I","J","K","L","M",
+    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m",
+    "n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "0","1","2","3","4","5","6","7","8","9",
+    "?",",","@","!","#","%","*",";","&","á","ü","æ","ø","®","¶","¼","€"
+
 ]
 
 var x = document.getElementById("text-zone");
@@ -12,7 +15,7 @@ var j = 0;
 var k = 0;
 var minus = false;
 var textWeightArray = [100,900];
-var textItalicArray = [0,10,-6];
+var textItalicArray = [10,-6];
 
 function changeText(){
     if (i < CharacterArrray.length){
@@ -23,12 +26,9 @@ function changeText(){
         i = 0;
     }
 
-    j++;
+    j++;k++;
     if(j == textWeightArray.length) j=0;
-
-    if (k == 2) minus = true;
-    if (k == 0) minus = false;
-    k = minus ? --k : ++k;
+    if(k == textItalicArray.length) k=0;
 
     x.style.fontVariationSettings = "\"wght\" " + textWeightArray[j] + ", \"ital\" " + textItalicArray[k];
 }
