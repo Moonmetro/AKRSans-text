b8e443a87e3f1a6adf4875ee820780c6b47619aa
Moonmetro <64722102+Moonmetro@users.noreply.github.com>
2023-12-14 17:46:27 +0800

    Add files via upload

--- animation-change.js
+++ animation-change.js
@@ -5,17 +5,22 @@ var CharacterArrray = [
     "Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x",
     "Y","y","Z","z",  
     "0","1","2","3","4","5","6","7","8","9",
-    "?",",","@","!","#","%","*",";","&","á","ü","æ","ø","®","¶","¼","€"
+    "?",",","@","!","#","%","*",";","&"
 
 ]
+
+var rgbRArray = [43,202,18,255,66,125,0];
+var rgbGArray = [107,189,41,87,166,191,0];
+var rgbBArray = [255,217,82,102,255,38,0];
 
 var x = document.getElementById("text-zone");
 var i = 0;
 var j = 0;
 var k = 0;
+// String colorstring = "rgb(" + rgbRArray[k] + "," + rgbGArray[k] + "," + rgbBArray[k] +
 var minus = false;
-var textWeightArray = [100,900];
-var textItalicArray = [10,-6];
+var textWeightArray = [56,952];
+// var textItalicArray = [10,-6];
 
 function changeText(){
     if (i < CharacterArrray.length){
@@ -26,11 +31,27 @@ function changeText(){
         i = 0;
     }
 
-    j++;k++;
+    j++;
     if(j == textWeightArray.length) j=0;
-    if(k == textItalicArray.length) k=0;
 
-    x.style.fontVariationSettings = "\"wght\" " + textWeightArray[j] + ", \"ital\" " + textItalicArray[k];
+    x.style.fontVariationSettings = "\"wght\" " + textWeightArray[j];
+    
 }
 
+function changeColor(){
+    if(k == 0) x.style.color = "rgb(43,107,255)";
+    else if(k == 1) x.style.color = "rgb(202,189,217)";
+    else if(k == 2) x.style.color = "rgb(18,41,82)";
+    else if(k == 3) x.style.color = "rgb(255,87,102)";
+    else if(k == 4) x.style.color = "rgb(66,166,255)";
+    else if(k == 5) x.style.color = "rgb(125,191,38)";
+    else if(k == 6) x.style.color = "rgb(0,0,0)";
+
+    if(k == 7) {x.style.color = "rgb(43,107,255)"; k = 0;}
+    k++;
+
+}
+
+
 setInterval(changeText, 1600);
+setInterval(changeColor, 1600);
